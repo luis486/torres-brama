e56c5ab508fda6a53ef472b3e290969c9a6be257
luis486 <lualmuhe@gmail.com>
2021-04-16 10:04:49 -0500

    hanoi towers completed

--- src/model/Torres.java
+++ src/model/Torres.java
@@ -1,5 +1,47 @@
 package model;
 
 public class Torres {
-    
+
+	private int torre1;
+	private int torre2;
+	private int torre3;
+
+	public Torres() {
+	}
+
+    public void hanoiTowers(int d) {
+		torre1 = d;
+		torre2 = 0;
+		torre3 = 0;
+		hanoiTowers(d,1,2,3);		
+	}	
+
+	private void hanoiTowers(int d, int t1, int t2, int t3) {
+		if(d==1) {
+			ifAnidados(t1, t3);
+			System.out.println(torre1 + " " + torre2 + " " + torre3);		
+		}else {
+			hanoiTowers((d-1),t1,t3,t2);	
+			ifAnidados(t1, t3);		
+			System.out.println(torre1 + " " + torre2 + " " + torre3);
+			hanoiTowers((d-1),t2,t1,t3);		
+		}
+	}
+
+	private void ifAnidados(int o, int d){
+		if(o == 1){
+			torre1--;
+		}else if(o == 2){
+			torre2--;
+		}else{
+			torre3--;
+		}
+		if(d == 1){
+			torre1++;
+		}else if(d == 2){
+			torre2++;
+		}else{
+			torre3++;
+		}
+	}
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,7 +1,31 @@
 package ui;
+import model.*;
+import java.util.Scanner;
 
 public class Main {
+
+  
+    private static Scanner sc = new Scanner(System.in);
+    private static Torres torres;
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        torres = new Torres();
+        initialize();
+    }
+
+    public static void initialize(){
+        System.out.println("Por favor ingrese el numero de casos y luego el numero de discos que desea");
+        int numberCases = Integer.parseInt(sc.nextLine());
+        int[] disk = new int[numberCases]; 
+        for(int i = 0; i < numberCases; i++){
+            disk[i] = Integer.parseInt(sc.nextLine());
+           
+        }
+        for(int j = 0; j < numberCases; j++){
+            System.out.println(disk[j] + " " + 0 + " " + 0);
+            torres.hanoiTowers(disk[j]);
+            System.out.println("");
+        }
+
     }
 }
